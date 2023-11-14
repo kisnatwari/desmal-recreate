9899811ada805fff2ad594cbc6bb2d3411c69a7b
Krishna Tiwari <kisnatwari@gmail.com>
2023-11-14 20:25:21 +0545

    Shadcn ui implemented

---
+++ src/components/global/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <div>
+            <span>देशमल </span>
+            <span>इलेक्ट्रिकल</span>
+        </div>
+    )
+}
+
+export default Logo

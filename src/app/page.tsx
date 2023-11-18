9e14a9ddde5fe08a062d763e5e97075c965bdbe5
Krishna Tiwari <kisnatwari@gmail.com>
2023-11-18 22:16:19 +0545

    Footer Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import BanneredProducts from '@/components/BanneredProducts'
 import TopRated from '@/components/TopRated'
 import Image from 'next/image'
 import AvailableProducts from '@/components/AvailableProducts'
+import Footer from '@/components/Footer'
 
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <TopRated />
       <BanneredProducts />
       <AvailableProducts />
+      <Footer />
     </main>
   )
 }
